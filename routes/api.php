5a5ef19dee9f45c6a749861d670b00d9fe494de3
Mateusz Szewczyk <m.szewczyk58@gmail.com>
2022-10-24 13:52:38 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BalanceCotroller;
+use App\Http\Controllers\ControllerController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\OperatorController;
+use App\Http\Controllers\ParkingController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\StopController;
+use App\Http\Controllers\VehicleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +25,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'drivers' => DriverController::class,
+    'balances' => BalanceCotroller::class,
+    'vehicles' => VehicleController::class,
+    'operators' => OperatorController::class,
+    'controllers' => ControllerController::class,
+    'parkings' => ParkingController::class,
+    'reservations' => ReservationController::class,
+    'stops' => StopController::class,
+]);
